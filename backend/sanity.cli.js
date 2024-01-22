7b388bb0094d9e3b4a7ae02bc64c342e73b89526
Juan Martinez <juan.martinez43@udea.edu.co>
2024-01-22 14:45:09 +0000

    change s

---
+++ backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '1qr8s6o2',
+    dataset: 'production'
+  }
+})
--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React from 'react';
+import { About, Footer, Header, Skills, Testimonials, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
-const App = () => {
-    return(
-        <h1>App</h1>
-    );
-}
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonials />
+    <Footer />
+  </div>
+);
 
 export default App;
